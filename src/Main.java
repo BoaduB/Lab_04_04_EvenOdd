9b4c6dc141413e5655801af4d07f294cf0fbee5b
BoaduB <boansibk@mail.uc.edu>
2023-02-02 02:02:47 -0500

    LAB_04_04_EVENODD

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int numToExamine = 5;
+
+        int remainderMod = 0;
+
+        remainderMod = numToExamine % 2;
+
+        System.out.println("Since " + numToExamine + " mod 2 is " + remainderMod + ", " + numToExamine + " is Odd.");
     }
 }
